a6b4e069dbcffbc158b0a8aa4d2b165ee41ad69d
Edward Gasparian <gilberd@i.ua>
2025-06-12 09:57:18 +0400

    Initial Commit

---
+++ test_task_ios2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_task_ios2
+//
+//  Created by Edward Gasparian on 12.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_task_ios2/test_task_ios2App.swift
@@ -0,0 +1,17 @@
+//
+//  test_task_ios2App.swift
+//  test_task_ios2
+//
+//  Created by Edward Gasparian on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct test_task_ios2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
